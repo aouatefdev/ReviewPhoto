12f6bb2e0cc13679b7ce8696fe0a3a6861334164
aouatefdev <aouatef.mech@gmail.com>
2023-01-23 16:09:27 +0100

    FIN DE PARTIE 2

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Photo;
 use DateTimeImmutable;
+use App\Repository\PhotoRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,18 +12,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PhotoController extends AbstractController
 {
     #[Route('/', name: 'photo.list')]
-   
-    public function list(): Response
+    public function list(PhotoRepository $photoRepository): Response
     {
         // return $this->render('photo/list.html.twig', [
         //     'controller_name' => 'PhotoController',
         // ]);
 
-        $photo = new Photo(); 
-        $photo->setTitle('Ma première photo'); 
-        $photo->setPostAt( new DateTimeImmutable()); 
-        $photos = [] ; 
-        $photos[] = $photo ; 
+        // $photo = new Photo(); 
+        // $photo->setTitle('Ma première photo'); 
+        // $photo->setPostAt( new DateTimeImmutable()); 
+        // $photos = [] ; 
+        // $photos[] = $photo ; 
+
+        $photos = $photoRepository->findAll(); 
 
         return $this->render('photo/list.html.twig', ['photos' => $photos]) ; 
 
